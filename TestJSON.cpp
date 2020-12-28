3791730eddf1b35ea13be62686ae8115794c2075
Alex Groce <agroce@gmail.com>
2020-12-28 09:42:51 -0700

    Include the null terminators

--- TestJSON.cpp
+++ TestJSON.cpp
@@ -7,13 +7,11 @@ using namespace deepstate;
 
 TEST(JSON, RoundTrip) {
   size_t size1 = DeepState_UIntInRange(0, MAX_LEN);
-  //Fails if we include numbers!  The next line should allow all tests to pass.
-  //char* str1 = DeepState_CStr(size1, ",\":{}[]abcdef\t\n ");
   char* str1 = DeepState_CStr(size1, ",\":{}[]abcdef0123456789E.-\t\n ");
   LOG(TRACE) << "String 1:" << str1;
   LOG(TRACE) << "Length 1:" << size1;
   json_tokener *tok1 = json_tokener_new();
-  json_object *obj1 = json_tokener_parse_ex(tok1, str1, size1);
+  json_object *obj1 = json_tokener_parse_ex(tok1, str1, size1 + 1);
   LOG(TRACE) << "Type 1:" << json_object_get_type(obj1);
   if (obj1 != NULL) {
     const char* str2 = json_object_to_json_string(obj1);
@@ -21,7 +19,7 @@ TEST(JSON, RoundTrip) {
     LOG(TRACE) << "String 2:" << str2;
     LOG(TRACE) << "Length 2:" << size2;
     json_tokener *tok2 = json_tokener_new();    
-    json_object *obj2 = json_tokener_parse_ex(tok2, str2, size2);
+    json_object *obj2 = json_tokener_parse_ex(tok2, str2, size2 + 1);
     ASSERT (obj2 != NULL) << "Object 2 should not be null!";
     LOG(TRACE) << "Type 2:" << json_object_get_type(obj2);
     ASSERT (json_object_equal(obj1, obj2)) << "NOT EQUAL:\nSTRING 1:\n" << str1 << "\nSTRING 2:\n" << str2;
